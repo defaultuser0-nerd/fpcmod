8af30664e35021a8d591ea07d88b1941fc6c86d6
defaultuser0 <axolotlslmao@gmail.com>
2024-12-01 11:14:32 -0600

    fwesdjk;

---
+++ Plugin.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+
+namespace fpc
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        void Start()
+        {
+            HarmonyPatches.ApplyHarmonyPatches();
+            GorillaTagger.OnPlayerSpawned(Initialized);
+        }
+
+        void Initialized()
+        {
+            GameObject.Find("Player Objects/Third Person Camera").SetActive(false);
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace fpc
+{
+    class PluginInfo
+    {
+        public const string GUID = "com.defaultuser0.fpc";
+        public const string Name = "fpc";
+        public const string Version = "1.0.0";
+    }
+}
